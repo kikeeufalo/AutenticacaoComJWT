c6368685374067510b65e3f35d2786733e1744ea
Wellinghton O. <71161234+kikeeufalo@users.noreply.github.com>
2025-03-16 17:53:49 -0300

    Create login.js

---
+++ FrontEnd/JS/login.js
@@ -0,0 +1,25 @@
+document.getElementById('login').addEventListener('submit', async function (event) {
+    event.preventDefault() // evita o carregamento da pagina
+
+    const email = document.getElementById('nome-email').value
+    const password = document.getElementById('senha').value
+
+    try {
+        const res = await fetch("http://localhost:3000/login", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password })
+        })
+
+        const data = await res.json()
+        if (res.ok) {
+            localStorage.setItem("token", data.token) // Salvando o token
+        } else {
+            alert("Erro: " + data.message)
+        }
+    } catch (error) {
+        console.log('Erro:', error)
+    }
+
+
+})
